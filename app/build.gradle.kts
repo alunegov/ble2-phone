cf7e0c5fe22922c65aec2c6b42f6b35d8b040bbc
Alexander Lunegov <alunegov@gmail.com>
2023-07-08 18:39:53 +0500

    chore: ver 0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.alunegov.ble2.android"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
